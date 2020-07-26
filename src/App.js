46be514bb063ec7310fa694c93110e1dd80a33c1
rahulbhati2304 <66083004+rahulbhati2304@users.noreply.github.com>
2020-07-26 09:12:15 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "./Header";
+import "./styles.css"
+
+function App(){
+    return (
+    <div className="Container">
+      <div className="Header">
+        <Header />
+      </div>
+    </div>
+    );
+  }
+
+export default App;
---
+++ src/Header.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const header = () => {
+  return <p>Header</p>;
+};
+
+export default header;
